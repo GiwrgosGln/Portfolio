3610f7bf07e54948413fed437451605ca73e5f70
GiwrgosGln <gergegl1999@gmail.com>
2024-08-26 21:05:08 +0300

    feat: Add loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { GradientBackground } from "@/components/gradient-background";
+import Loader from "./components/Loader";
 
 export default function Home() {
   return (
     <>
-      <GradientBackground />
+      <Loader />
     </>
   );
 }
