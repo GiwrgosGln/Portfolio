f628ab5cc946e4e7f6682245f30cae459bbf5d8e
GiwrgosGlinias <gergegl1999@gmail.com>
2024-12-25 00:02:22 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       colors: {
         primary: "#111418",
-        secondary: "#6C83BF",
+        secondary: "#5C87F6",
         primaryText: "#9499AF",
         secondaryText: "#FFFFFF",
         textBackground: "#191E2C",
